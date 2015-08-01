1022c20abd4120aab793f6153bf0b9f167f420ba
Bereket Ghebremedhin <bereket6725@gmail.com>
2015-07-31 21:40:38 -0400

    first commit

---
+++ pokemonPracticeProject/pokemonPracticeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  pokemonPracticeTableViewController.h
+//  pokemonPracticeProject
+//
+//  Created by Bereket  on 7/31/15.
+//  Copyright (c) 2015 Bereket . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pokemonPracticeTableViewController : UITableViewController
+
+@end
